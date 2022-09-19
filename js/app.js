65c3ce8de21f7dd5b0c74ea4b9d3847034162211
yulsep <yulimarcela99@gmail.com>
2022-09-19 14:44:48 -0500

    Primeros archivos clase 2

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let unNumero=5;
+
+if (unNumero>10) {
+    alert("Numero alto")
+}else{
+    alert("Numero menor a 10")
+}
+
+let nombre = promp("Ingrese su nombre");
+if (nombre = ""){
+    alert("Por favor ingresa un nombre");
+}else{
+    alert("Hola señor " + nombre);
+}
+
+let myAge = promp("Ingrese su edad")
